8ad77bd5edd76135356b52b61a35b4f9beac1fc9
Hellmann Jens <Jens.Hellmann@hannover-re.com>
2018-09-12 15:46:24 +0200

    Static code analysis refactoring

--- CpuPaint.cs
+++ CpuPaint.cs
@@ -5,65 +5,62 @@ using System.Windows.Forms;
 
 namespace CpuMonitor
 {
-    /// <summary>
-    /// Zusammenfassung für CpuPaint.
-    /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Cpu")]
-    public class CpuPaint : IDisposable
+  /// <summary>
+  /// Zusammenfassung für CpuPaint.
+  /// </summary>
+  [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Cpu")]
+  public sealed class CpuPaint : IDisposable
   {
     /// <summary>
     /// Anzahl der angezeigten Maßeinheiten-Zeilen.
     /// </summary>
-    private const int MAX_ROWS = 10;
+    private const int MaxRows = 10;
 
     /// <summary>
     /// Anzahl der Pixel zwischen den Messpunkten im Diagrammfenster.
     /// </summary>
-    private const int PIX_DELTA_X_COORDS = 10;
+    private const int PixDeltaXCoords = 10;
 
     private readonly Control mControl;
     private readonly List<int> pointList = new List<int>();
-    private readonly Pen mGraphPen;
+    private readonly Pen graphPen;
     private readonly Pen mBackgroundLinesPen = new Pen(Color.DimGray, 1);
 
-    private float mUsage = 0f;
+    private float currentProcessorUsage;
 
     /// <summary>
     /// Konstruktor.
     /// </summary>
-    /// <param name="pControl">Das Control, auf dem gezeichnet wird.</param>
-    /// <param name="penColor">Der Pen, mit dem der Graph gezeichnet wird.</param>
-    public CpuPaint(Control pControl, Pen graphPen)
+    /// <param name="hostControl">Das Control, auf dem gezeichnet wird.</param>
+    /// <param name="graphPen">Der Pen, mit dem der Graph gezeichnet wird.</param>
+    public CpuPaint(Control hostControl, Pen graphPen)
     {
-      this.mControl = pControl;
-      this.mControl.Paint += new PaintEventHandler(this.ControlPaint);
+      this.mControl = hostControl;
+      this.mControl.Paint += this.ControlPaint;
 
-      this.mGraphPen = graphPen;
+      this.graphPen = graphPen;
     }
 
 
     public void DrawGraph(float pUsage)
     {
-      this.mUsage = pUsage;
+      this.currentProcessorUsage = pUsage;
     }
 
 
-    private void DrawLines(Graphics pGraphics, int pControlHeight)
+    private void DrawLines(Graphics graphics)
     {
-      float controlHeight = pGraphics.VisibleClipBounds.Height;
-      float rowHeight = controlHeight / MAX_ROWS;
+      float controlHeight = graphics.VisibleClipBounds.Height;
+      float rowHeight = controlHeight / MaxRows;
 
-      Point lPointLeft;
-      Point lPointRight;
-
-      for (int i = 1; i < MAX_ROWS; i++)
+        for (int i = 1; i < MaxRows; i++)
       {
         int yOffset = (int)(rowHeight * i);
 
-        lPointLeft = new Point(0, yOffset);
-        lPointRight = new Point(this.mControl.Width, yOffset);
+        var pointLeft = new Point(0, yOffset);
+        var pointRight = new Point(this.mControl.Width, yOffset);
 
-        pGraphics.DrawLine(this.mBackgroundLinesPen, lPointLeft, lPointRight);
+          graphics.DrawLine(this.mBackgroundLinesPen, pointLeft, pointRight);
       }
     }
 
@@ -75,38 +72,35 @@ namespace CpuMonitor
     /// <param name="pPointList"></param>
     private void DrawUsage(Graphics pGraphics, List<int> pPointList)
     {
-      int lXCoordinate = pPointList.Count * PIX_DELTA_X_COORDS;
+      int lXCoordinate = pPointList.Count * PixDeltaXCoords;
 
       if (lXCoordinate > this.mControl.ClientSize.Width)
       {
         pPointList.RemoveAt(0);
       }
 
-      Point lStartPoint = new Point(0, (int)pPointList[0]);
-      Point lNextPoint;
+      Point startPoint = new Point(0, pPointList[0]);
 
       for (int i = 0; i < this.pointList.Count; i++)
       {
-        lNextPoint = new Point(i * PIX_DELTA_X_COORDS, (int)pPointList[i]);
-        pGraphics.DrawLine(this.mGraphPen, lStartPoint, lNextPoint);
-        lStartPoint = lNextPoint;
+        var nextPoint = new Point(i * PixDeltaXCoords, pPointList[i]);
+        pGraphics.DrawLine(this.graphPen, startPoint, nextPoint);
+        startPoint = nextPoint;
       }
     }
 
     private void ControlPaint(object sender, PaintEventArgs e)
     {
       Graphics g = e.Graphics;
-      Region lRegion = g.Clip;
-      int lHeight = this.mControl.ClientSize.Height;
 
-      //g.FillRegion( new SolidBrush( Color.Black ), lRegion );
+      var lHeight = this.mControl.ClientSize.Height;
 
-      this.DrawLines(g, lHeight);
+      this.DrawLines(g);
 
       if (lHeight != 0)
       {
         float lTempPercent = (float)lHeight / 100;
-        lHeight = (int)(lTempPercent * this.mUsage);
+        lHeight = (int)(lTempPercent * this.currentProcessorUsage);
       }
 
       int lYCoordinate = this.mControl.ClientSize.Height - lHeight;
@@ -121,7 +115,7 @@ namespace CpuMonitor
     /// <summary>
     /// Gibt an, ob Dispose() vom User/ User-Code oder durch die Runtime (Destruktor) aufgerufen wurde.
     /// </summary>
-    private bool m_Disposed;
+    private bool disposed;
 
     /// <summary>
     /// Implementiert IDisposable.
@@ -150,10 +144,10 @@ namespace CpuMonitor
     /// </summary>
     /// <param name="disposing">TRUE, wenn die Methode durch User-Code aufgerufen wurde oder
     /// FALSE, wenn die Methode durch die Runtime aufgerufen wurde.</param>
-    protected void Dispose(bool disposing)
+    public void Dispose(bool disposing)
     {
       // Prüfen, ob Dispose() bereits aufgerufen wurde.
-      if (!this.m_Disposed)
+      if (!this.disposed)
       {
         // Wenn disposing == TRUE, gib alle managed und unmanaged Resourcen frei.
         if (disposing)
@@ -163,14 +157,14 @@ namespace CpuMonitor
 
           this.mControl?.Dispose();
           this.mBackgroundLinesPen?.Dispose();
-          this.mGraphPen?.Dispose();
+          this.graphPen?.Dispose();
         }
 
         // Hier UNMANAGED Resourcen freigeben.
         // Wenn disposing == false, wird nur der folgende Code ausgeführt.
 
         // Speichern, dass Dispose fertig abgearbeitet wurde.
-        this.m_Disposed = true;
+        this.disposed = true;
       }
     }
 
